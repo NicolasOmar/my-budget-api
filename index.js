0a9abdf572f969c2de6ed88d26096e71e13c4c1c
Nicolás Omar González Passerino <nicolas.passerino@gmail.com>
2020-03-17 12:43:32 -0300

    Heroku connection issues fix

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const chalk = require('chalk')
 // REQUIRE ALL 'MONGOOSE' CODE
-require('./db/mongoose')
+require('./src/db/mongoose')
 // IMPORT AND SET EXPRESS SERVER
 const app = express()
 const port = process.env.PORT
 // IMPORT ROUTERS
-const usersRouter = require('./routes/users')
+const usersRouter = require('./src/routes/users')
 
 // MANAGE EXPRESS SERVER USING JSON
 app.use(express.json())
@@ -21,7 +20,5 @@ app.get('/',
 
 app.listen(
   port,
-  () => chalk ?
-    console.log(chalk.white.bold.bgGreen(`Server up and working on port ${port}`)) :
-    console.log(`Server up and working on port ${port}`)
+  () => console.log(`Server up and working on port ${port}`)
 )
